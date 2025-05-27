465c6f399ba3bbc7122b6a051017c1f7deb4e533
Anto Francis <antojason25@gmail.com>
2025-05-27 16:51:44 -0400

    test commit

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,8 @@ import os
 
 app = Flask(__name__, static_folder='../frontend', static_url_path='')
 
+# testing the new branch
+
 @app.route('/recommend', methods=['POST'])
 def recommend_gear():
     data = request.get_json()
