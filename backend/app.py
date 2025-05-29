9a77ec97ecc795e3d54916698bfc9febd1fc083c
Anto Francis <antojason25@gmail.com>
2025-05-29 02:09:46 -0400

    omg

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, send_from_directory
 import os, random
 
-app = Flask(__name__, static_folder='../frontend', static_url_path='')
+app = Flask(__name__, static_folder='frontend', static_url_path='')
 
 # testing the new branch. New change
 
